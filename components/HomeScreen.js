6ae0c048d0aa84e5d8a1b968c5cc83ef1fb51405
Cadesp <124199026+Cadesp@users.noreply.github.com>
2023-03-03 13:04:28 -0600

    Update

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as ScreenOrientation from 'expo-screen-orientation';
 import { StyleSheet, Text, View, SafeAreaView, Button, TextInput, ScrollView } from 'react-native';
+import Checkbox from 'expo-checkbox';
 
 
 export default function HomeScreen({ navigation }) {
@@ -34,56 +35,7 @@ export default function HomeScreen({ navigation }) {
         textAlign="center" />
       <View style={styles.checkInput}>
         <Text>Overtime</Text>
-        <Checkbox style={{ marginLeft: 25 }} value={natQualH} onValueChange={setNatQualH} />
       </View>
-
-    </View><Text style={styles.titleText}>Opponent</Text><View style={styles.mainSection}>
-        <TextInput
-          style={styles.input}
-          placeholder='First Name'
-          placeholderTextColor='black'
-          textAlign='center' />
-        <TextInput
-          style={styles.input}
-          placeholder='Last Name'
-          placeholderTextColor='black'
-          textAlign='center' />
-        <TextInput
-          style={styles.input}
-          placeholder="Team"
-          placeholderTextColor="black"
-          textAlign="center" />
-        <TextInput
-          style={styles.input}
-          placeholder="Weight Class"
-          placeholderTextColor="black"
-          textAlign="center" />
-        <View style={styles.checkInput}>
-          <Text>NAT QUAL</Text>
-          <Checkbox style={{ marginLeft: 25 }} value={natQualA} onValueChange={setNatQualA} />
-        </View>
-        <View style={styles.checkInput}>
-          <Text>All American</Text>
-          <Checkbox style={{ marginLeft: 25 }} value={allAmerA} onValueChange={setAllAmerA} />
-        </View>
-        <View style={{ padding: 20 }}>
-          <TextInput
-            style={styles.input}
-            placeholder='Event Name'
-            placeholderTextColor='black'
-            textAlign='center' />
-          <TextInput
-            style={styles.input}
-            placeholder='Event Type'
-            placeholderTextColor='black'
-            textAlign='center' />
-          <TextInput
-            style={styles.input}
-            placeholder='Match Type'
-            placeholderTextColor='black'
-            textAlign='center' />
-        </View>
-
       </View><Button title="Submit" onPress={() => navigation.navigate('EventPage', { paramKey: nameH })} /></>
   );
 }
