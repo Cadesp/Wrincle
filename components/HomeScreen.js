eb2fb21a5b1f0addc8e2895a5a7811fe59c4af80
Cadesp <124199026+Cadesp@users.noreply.github.com>
2023-03-03 12:54:19 -0600

    updating

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as ScreenOrientation from 'expo-screen-orientation';
