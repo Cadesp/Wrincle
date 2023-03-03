5bb3f5ae60ab63012372a42519c04fc5e56a9285
Cadesp <124199026+Cadesp@users.noreply.github.com>
2023-03-03 12:45:34 -0600

    Updating New Match Page

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -8,10 +8,100 @@ import * as ScreenOrientation from 'expo-screen-orientation';
 export default function HomeScreen({ navigation }) {
   ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
   return (
+    <ScrollView contentContainerStyle={styles.container} keyboardShouldPersistTaps='handled'>
+    <Text style={styles.titleText}>Match Summary</Text>
     <View style={styles.mainSection}>
-      <Text>Home Screen</Text>
-      <Button title="Create New Match" onPress={() => navigation.navigate('NewMatch')}/>
+      <TextInput 
+          style={styles.input}
+          placeholder='Time'
+          placeholderTextColor='black'
+          textAlign='center'
+          value={timeH}
+          onChangeText={(timeH) => setNameH(timeH)}
+          
+      />
+      <TextInput 
+          style={styles.input}
+          placeholder='Position'
+          placeholderTextColor='black'
+          textAlign='center'
+      />
+      <TextInput 
+          style={styles.input}
+          placeholder="Riding Time"
+          placeholderTextColor="black"
+          textAlign="center"
+      />
+      <TextInput 
+          style={styles.input}
+          placeholder="Period"
+          placeholderTextColor="black"
+          textAlign="center"
+      />
+      <View style={styles.checkInput}>
+      <Text>Overtime</Text>
+      <Checkbox style={{marginLeft: 25}} value={natQualH} onValueChange={setNatQualH}/> 
+      </View>
+      
     </View>
+    <Text style={styles.titleText}>Opponent</Text>
+    <View style={styles.mainSection}>
+      <TextInput 
+          style={styles.input}
+          placeholder='First Name'
+          placeholderTextColor='black'
+          textAlign='center' 
+      />
+      <TextInput 
+          style={styles.input}
+          placeholder='Last Name'
+          placeholderTextColor='black'
+          textAlign='center'
+      />
+      <TextInput 
+          style={styles.input}
+          placeholder="Team"
+          placeholderTextColor="black"
+          textAlign="center"
+      />
+      <TextInput 
+          style={styles.input}
+          placeholder="Weight Class"
+          placeholderTextColor="black"
+          textAlign="center"
+      />
+      <View style={styles.checkInput}>
+          <Text>NAT QUAL</Text>
+          <Checkbox style={{marginLeft: 25}} value={natQualA} onValueChange={setNatQualA}/> 
+      </View>
+      <View style={styles.checkInput}>
+          <Text>All American</Text>
+          <Checkbox style={{marginLeft: 25}} value={allAmerA} onValueChange={setAllAmerA}/>
+      </View>
+      <View style={{padding: 20}}>
+      <TextInput 
+          style={styles.input}
+          placeholder='Event Name'
+          placeholderTextColor='black'
+          textAlign='center' 
+      />
+      <TextInput 
+          style={styles.input}
+          placeholder='Event Type'
+          placeholderTextColor='black'
+          textAlign='center' 
+      />
+      <TextInput 
+          style={styles.input}
+          placeholder='Match Type'
+          placeholderTextColor='black'
+          textAlign='center' 
+      />
+      </View>
+
+    </View>
+    <Button title="Submit" onPress={() => navigation.navigate('EventPage', {paramKey: nameH})}/>
+  </ScrollView>
   );
 }
 
