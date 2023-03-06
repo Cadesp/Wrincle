6189d07d345a80e7264d3564f8fc292f29b773fa
Cadesp <124199026+Cadesp@users.noreply.github.com>
2023-03-06 13:02:25 -0600

    Update

--- components/EventPage.js
+++ components/EventPage.js
@@ -19,96 +19,16 @@ export default function EventPage({ route, navigation }) {
         <Text style='alignItems: '>Match Summary {route.params.paramKey} </Text>
         <Button title="Back to Home" onPress={() => { navigation.navigate('HomeScreen'); ScreenOrientation.unlockAsync(); } } />
       </View>
-    </SafeAreaView><ScrollView contentContainerStyle={styles.container} keyboardShouldPersistTaps='handled'>
-        <Text style={styles.titleText}>Our Wrestler</Text>
-        <View style={styles.mainSection}>
-          <TextInput
-            style={styles.input}
-            placeholder='First Name'
-            placeholderTextColor='black'
-            textAlign='center'
-            value={nameH}
-            onChangeText={(nameH) => setNameH(nameH)} />
-          <TextInput
-            style={styles.input}
-            placeholder='Last Name'
-            placeholderTextColor='black'
-            textAlign='center' />
-          <TextInput
-            style={styles.input}
-            placeholder="Team"
-            placeholderTextColor="black"
-            textAlign="center" />
-          <TextInput
-            style={styles.input}
-            placeholder="Weight Class"
-            placeholderTextColor="black"
-            textAlign="center" />
-          <View style={styles.checkInput}>
-            <Text>NAT QUAL</Text>
-            <Checkbox style={{ marginLeft: 25 }} value={natQualH} onValueChange={setNatQualH} />
-          </View>
-          <View style={styles.checkInput}>
-            <Text>All American</Text>
-            <Checkbox style={{ marginLeft: 25 }} value={allAmerH} onValueChange={setAllAmerH} />
-          </View>
-        </View>
-        <Text style={styles.titleText}>Opponent</Text>
-        <View style={styles.mainSection}>
-          <TextInput
-            style={styles.input}
-            placeholder='First Name'
-            placeholderTextColor='black'
-            textAlign='center' />
-          <TextInput
-            style={styles.input}
-            placeholder='Last Name'
-            placeholderTextColor='black'
-            textAlign='center' />
-          <TextInput
-            style={styles.input}
-            placeholder="Team"
-            placeholderTextColor="black"
-            textAlign="center" />
-          <TextInput
-            style={styles.input}
-            placeholder="Weight Class"
-            placeholderTextColor="black"
-            textAlign="center" />
-          <View style={styles.checkInput}>
-            <Text>NAT QUAL</Text>
-            <Checkbox style={{ marginLeft: 25 }} value={natQualA} onValueChange={setNatQualA} />
-          </View>
-          <View style={styles.checkInput}>
-            <Text>All American</Text>
-            <Checkbox style={{ marginLeft: 25 }} value={allAmerA} onValueChange={setAllAmerA} />
-          </View>
-          <View style={{ padding: 20 }}>
-            <TextInput
-              style={styles.input}
-              placeholder='Event Name'
-              placeholderTextColor='black'
-              textAlign='center' />
-            <TextInput
-              style={styles.input}
-              placeholder='Event Type'
-              placeholderTextColor='black'
-              textAlign='center' />
-            <TextInput
-              style={styles.input}
-              placeholder='Match Type'
-              placeholderTextColor='black'
-              textAlign='center' />
-          </View>
-
-        </View>
-        <Button title="Submit" onPress={() => navigation.navigate('EventPage', { paramKey: nameH })} />
-      </ScrollView></>
-        
+    </SafeAreaView>
+    <TextInput
+        style={styles.input}
+        placeholder='Event Name'
+        placeholderTextColor='black'
+        textAlign='center' />
+        </>
     
   );
-  };
-
+}
 
 const styles = StyleSheet.create({
   container: {
