01dc584711a0c98500a35379d56ef7726f128482
Cadesp <124199026+Cadesp@users.noreply.github.com>
2023-03-22 12:44:57 -0500

    Updating

--- components/EventPage.js
+++ components/EventPage.js
@@ -14,37 +14,40 @@ export default function EventPage({ route, navigation }) {
   const insets = useSafeAreaInsets();
   ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
 
-  const EventPage = ( { navigation } ) => {
+  // const EventPage = ( { navigation } ) => {
+    const [periodH, setPeriodH] = useState('1');
+    const [perioda, setPeriodA] = useState('1');
     const [isPositionH, setPositionH] = useState('Top');
     const [isPositionA, setPositionA] = useState('Top');
-  }
 
   return (
     <><SafeAreaView style={styles.safeView}>
       <View style={styles.container}>
         <Text style='alignItems: '>Match Summary {route.params.paramKey} </Text>
-        <Button title="Back to Home" onPress={() => { navigation.navigate('HomeScreen'); ScreenOrientation.unlockAsync(); } } />
       </View>
+      
       <TextInput 
         style={styles.input}
         placeholder= "Time"
         placeholderTextColor = 'black'
         textAlign='center' />
         
-        <SelectList 
+              <SelectList 
                 setSelected={(isPositionH) => setPositionH(isPositionH)}
                 data={[{label: 'Top', value: 'Top'}, {label: 'Bottom', value: 'Bottom'}, {label: "Neutral", value: 'Neutral'}]}
                 save="value"
                 placeholder={'Select Position'}
-                boxStyles={{
-                    width: 150, 
-                    height: 45, 
-                    backgroundColor: 'transparent', 
-                    borderRadius: 0, 
-                    borderWidth: 1, 
-                    borderColor: 'black', 
-                    margin: 20,
-                }}
+                style={styles.input}
+            />
+
+              <SelectList 
+                setSelected={(periodH) => setPeriodH(periodH)}
+                data={[{label: '1', value: '1'}, {label: '2', value: '2'}, {label: '3', value: '3'}]}
+                save="value"
+                placeholder={'Select Period'}
+                style={styles.input}
+                   
+                
             />
     
         <TextInput 
@@ -66,7 +69,6 @@ export default function EventPage({ route, navigation }) {
         textAlign='center' />
     </SafeAreaView>
         </>
-    
   );
 }
 
@@ -109,7 +111,7 @@ const styles = StyleSheet.create({
     width: 100,
     height: 35,
     margin: 10,
-    borderWidth: 2,
+    borderWidth: 3,
     padding: 10,
     fontColor: 'black',
   },
