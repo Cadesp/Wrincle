a7de1722e4dd005d85bfd8f02f7b6387288e714a
Cadesp <124199026+Cadesp@users.noreply.github.com>
2023-03-22 13:19:52 -0500

    Updating

--- components/EventPage.js
+++ components/EventPage.js
@@ -19,35 +19,33 @@ export default function EventPage({ route, navigation }) {
     const [perioda, setPeriodA] = useState('1');
     const [isPositionH, setPositionH] = useState('Top');
     const [isPositionA, setPositionA] = useState('Top');
+    const [otH, setOtH] = useState(false);
 
   return (
     <><SafeAreaView style={styles.safeView}>
       <View style={styles.container}>
-        <Text style='alignItems: '>Match Summary {route.params.paramKey} </Text>
       </View>
       
-      <TextInput 
+      <TextInput
         style={styles.input}
         placeholder= "Time"
         placeholderTextColor = 'black'
         textAlign='center' />
         
-              <SelectList 
-                setSelected={(isPositionH) => setPositionH(isPositionH)}
-                data={[{label: 'Top', value: 'Top'}, {label: 'Bottom', value: 'Bottom'}, {label: "Neutral", value: 'Neutral'}]}
-                save="value"
-                placeholder={'Select Position'}
-                style={styles.input}
+      <SelectList 
+        setSelected={(isPositionH) => setPositionH(isPositionH)}
+        data={[{label: 'Top', value: 'Top'}, {label: 'Bottom', value: 'Bottom'}, {label: "Neutral", value: 'Neutral'}]}
+        save="value"
+        placeholder={'Position'}
+        boxStyles={styles.selectList}
             />
 
               <SelectList 
                 setSelected={(periodH) => setPeriodH(periodH)}
                 data={[{label: '1', value: '1'}, {label: '2', value: '2'}, {label: '3', value: '3'}]}
                 save="value"
-                placeholder={'Select Period'}
-                style={styles.input}
-                   
-                
+                placeholder={"Period"}
+                boxStyles={styles.selectList}
             />
     
         <TextInput 
@@ -55,18 +53,12 @@ export default function EventPage({ route, navigation }) {
         placeholder= "Riding Time"
         placeholderTextColor = 'black'
         textAlign='center' />
-        
-        <TextInput 
-        style={styles.input}
-        placeholder= "Period"
-        placeholderTextColor = 'black'
-        textAlign='center' />
 
-        <TextInput 
-        style={styles.input}
-        placeholder= "Overtime"
-        placeholderTextColor = 'black'
-        textAlign='center' />
+  <View style={styles.checkInput}>
+        <Text>Overtime</Text>
+        <Checkbox style={{marginLeft: 10}} value={otH} onValueChange={setOtH}/>
+        </View>
+
     </SafeAreaView>
         </>
   );
@@ -88,7 +80,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flexWrap: 'wrap',
         flexDirection: 'row',
-        alignItems: 'center', 
+        alignItems: 'center',
         gap: '10rem',
         justifyContent: 'center',
 
@@ -107,22 +99,32 @@ const styles = StyleSheet.create({
     color: "white",
   },
   input: {
-    alignItems: 'stretch',
+    alignItems: 'center',
     width: 100,
-    height: 35,
+    height: 40,
     margin: 10,
-    borderWidth: 3,
+    borderWidth: 1,
     padding: 10,
     fontColor: 'black',
   },
 checkInput: {
-    alignItems: 'stretch',
-    width: 150,
+    alignItems: 'center',
+    width: 105,
     height: 40,
-    margin: 12,
+    margin: 10,
     borderWidth: 1,
     padding: 10,
     fontColor: 'black',
     flexDirection: 'row',
+},
+
+selectList: {
+  width: 100,
+  height: 40,
+  backgroundColor: 'transparent',
+  borderRadius: 0,
+  borderWidth: 1, 
+  borderColor: 'black', 
+  margin: 10,
 }
 });
